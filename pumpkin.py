355619a16b596494c2b8e102b68708d638de735a
Matt Cronin <jmpcronin@gmail.com>
2017-10-19 11:21:10 -0400

    Create pumpkin.py

---
+++ pumpkin.py
@@ -0,0 +1,42 @@
+from sense_hat import SenseHat
+from time import sleep
+
+start_humidity = sense.humidity
+
+o = (255, 165, 0)
+r = (255, 0, 0)
+w = (255, 255, 255)
+g = (0, 128, 0)
+b = (0, 0, 0)
+
+light = [
+    w,w,w,w,g,w,w,w,
+    w,w,o,g,o,o,w,w,
+    w,o,o,o,o,o,o,w,
+    o,o,r,o,o,r,o,o,
+    o,o,o,o,o,o,o,o,
+    w,o,o,r,r,o,o,w,
+    w,w,o,o,o,o,w,w,
+    w,w,w,o,o,w,w,w,
+]
+
+nolight = [
+    w,w,w,w,g,w,w,w,
+    w,w,o,g,o,o,w,w,
+    w,o,o,o,o,o,o,w,
+    o,o,b,o,o,b,o,o,
+    o,o,o,o,o,o,o,o,
+    w,o,o,b,b,o,o,w,
+    w,w,o,o,o,o,w,w,
+    w,w,w,o,o,w,w,w
+
+
+while True:
+    print(sense.humidity)
+    if sense.humidity > start_humidity + 10:
+        sense.set_pixels(happy)
+    elif sense.humidity > start_humidity + 5:
+        sense.set_pixels(normal)
+    else:
+        sense.set_pixels(light)
+    sleep(1)
