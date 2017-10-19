7405afe8e559b1b238ed2d48df634c8bee6f2b82
jmpcronin <jmpcronin@gmail.com>
2017-10-19 12:38:13 -0500

    added black background

--- pumpkin.py
+++ pumpkin.py
@@ -1,6 +1,7 @@
 from sense_hat import SenseHat
 from time import sleep
 
+sense = SenseHat()
 start_humidity = sense.humidity
 
 o = (255, 165, 0)
@@ -10,9 +11,9 @@ g = (0, 128, 0)
 b = (0, 0, 0)
 
 light = [
-    w,w,w,w,g,w,w,w,
-    w,w,o,g,o,o,w,w,
-    w,o,o,o,o,o,o,w,
+    b,b,b,b,g,b,b,b,
+    b,b,o,g,o,o,b,b,
+    b,o,o,o,o,o,o,b,
     o,o,r,o,o,r,o,o,
     o,o,o,o,o,o,o,o,
     w,o,o,r,r,o,o,w,
@@ -28,8 +29,8 @@ nolight = [
     o,o,o,o,o,o,o,o,
     w,o,o,b,b,o,o,w,
     w,w,o,o,o,o,w,w,
-    w,w,w,o,o,w,w,w
-
+    w,w,w,o,o,w,w,w,
+]
 
 while True:
     print(sense.humidity)
