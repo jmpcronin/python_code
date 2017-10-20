0872db96a85e1bec26b4bb8e49a438cfc05d5c52
Matt Cronin <jmpcronin@gmail.com>
2017-10-20 08:36:20 -0400

    Create pumpkin2.py

---
+++ pumpkin2.py
@@ -0,0 +1,45 @@
+from sense_hat import SenseHat
+from time import sleep
+
+sense = SenseHat()
+sense = clear
+start_temp = sense.temperature
+
+o = (255, 165, 0)
+r = (255, 0, 0)
+w = (255, 255, 255)
+g = (0, 128, 0)
+b = (0, 0, 0)
+
+light = [
+    b,b,b,b,g,b,b,b,
+    b,b,o,g,o,o,b,b,
+    b,o,o,o,o,o,o,b,
+    o,o,r,o,o,r,o,o,
+    o,o,o,o,o,o,o,o,
+    b,o,o,r,r,o,o,b,
+    b,b,o,o,o,o,b,b,
+    b,b,o,o,o,o,b,b,
+]
+
+nolight = [
+    b,b,b,b,g,b,b,b,
+    b,b,o,g,o,o,b,b,
+    b,o,o,o,o,o,o,b,
+    o,o,b,o,o,b,o,o,
+    o,o,o,o,o,o,o,o,
+    b,o,o,b,b,o,o,b,
+    b,b,o,o,o,o,b,b,
+    b,b,o,o,o,o,b,b,
+]
+
+
+while True:
+    print(sense.temperature)
+    if sense.temperature > start_temp + 10:
+        sense.set_pixels(nolight)
+    elif sense.temperature > start_temp + 5:
+        sense.set_pixels(nolight)
+    else:
+        sense.set_pixels(light)
+    sleep(1)
